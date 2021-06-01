8eb9fb2763279406c52dbea63229b5a396e093b6
John McNay <John.McNay@improving.com>
2021-06-01 08:25:02 -0500

    initial commit

---
+++ knock-knock/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace knock_knock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string response;
+
+            do
+            {
+                Console.WriteLine("Knock knock!");
+                response = Console.ReadLine();
+
+            } while (isInvalidResponse(response));
+
+
+
+
+
+
+
+        }
+
+        private static bool isInvalidResponse(string response)
+        {
+            return (response != "Who's there?");
+        }
+    }
+}
